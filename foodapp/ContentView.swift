14f6ef3691f13eb06e87547d6aa55d16bb66c4e7
williambeesley <wpbeesley@gmail.com>
2026-03-27 22:47:58 -0700

    Initial Commit

---
+++ foodapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  foodapp
+//
+//  Created by William Pan-Beesley on 3/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ foodapp/foodappApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodappApp.swift
+//  foodapp
+//
+//  Created by William Pan-Beesley on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct foodappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
